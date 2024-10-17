8a37205570d3491a10e82977c33b6213b6ed1a75
mariaaa-11 <songanomariarose534.pnc@gmail.com>
2024-10-17 13:46:11 +0800

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 const About = () => {
   return (
     <Container>
-      <h1>About Me</h1>
+        <h1 className="custom-violet">About Me</h1>
       <p> I am 20 Years old and a  3rd-year Bachelor of Science in Computer Science (BSCS) student with a strong passion for playing volleyball. </p>
       <p> </p>
     </Container>
