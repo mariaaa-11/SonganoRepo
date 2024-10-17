90f3fd7a19400b50ad0cbbdfc84d2978e19eeec0
mariaaa-11 <songanomariarose534.pnc@gmail.com>
2024-10-17 14:21:19 +0800

    Updated SubmissionSuccess.js

--- src/components/SubmissionSuccess.js
+++ src/components/SubmissionSuccess.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 const SubmissionSuccess = ({ name, email, message }) => {
   return (
-    <div>
+    <Container>
+  
       <h2></h2>
-     <h2 className="text-primary text-center">Message Sent successfully .</h2>......
+     <h2 className="text-primary text-center">Message Sent successfully .</h2>
+    
     <ul>
       <li style={listItemStyle}><strong>Name:</strong> {formData.name}</li>
   </ul>
-    </div>
+  
+  </Container>
   );
 };
 
